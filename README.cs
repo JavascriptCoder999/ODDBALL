b8ae5e2d65d8c0c7588c418c7fd6d71e156a5e8a
FuturamaGuy2017 <81769195+JavascriptCoder999@users.noreply.github.com>
2021-05-01 13:58:30 +0100

    Create README.cs

---
+++ README.cs
@@ -0,0 +1,6 @@
+# ODDBALL
+//add a background (big rectangle)
+//add a scene
+//add an oddball go group containing hands, shirt, head and an oddball body base
+//run the .js file
+//report issue if there is one (or more)
