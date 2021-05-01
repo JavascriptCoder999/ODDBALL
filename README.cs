bcae5090440ce03c2300671e0d8a975b73698c8c
FuturamaGuy2017 <81769195+JavascriptCoder999@users.noreply.github.com>
2021-05-01 14:00:14 +0100

    Update README.cs

--- README.cs
+++ README.cs
@@ -4,3 +4,6 @@
 //add an oddball go group containing hands, shirt, head and an oddball body base
 //run the .js file
 //report issue if there is one (or more)
+//how to game it:
+//there should be nine images of anything you like (insert them using piskel art software)
+//pick the odd one out
